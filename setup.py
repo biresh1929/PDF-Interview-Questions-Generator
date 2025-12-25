d8bd1e6c2389114fe0bce9d754136fd3a6469250
biresh1929 <bireshkumar1964@gmail.com>
2025-12-25 21:57:45 +0530

    fix: add langchain-huggingface dependency

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     author="Biresh Kumar Singh",
     author_email="bireshkumar1964@gmail.com",
     packages=find_packages(),
-    install_requires=[ "langchain-groq",]
+    install_requires=[ "langchain-groq","langchain-huggingface"]
 )
